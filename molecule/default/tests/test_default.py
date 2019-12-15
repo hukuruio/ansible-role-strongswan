d5c83b42a420f6385bea0833c72315b82e0dee89
Don Nyambudzi <don.nyambudzi@gmail.com>
2019-12-15 13:30:57 +0000

    builds basic config

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,7 +6,34 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']
 ).get_hosts('all')
 
+component_name = "strongswan"
+
 
 def test_strongswan_is_installed(host):
-    strongswan = host.package('strongswan')
+    strongswan = host.package(component_name)
     assert strongswan.is_installed
+
+
+def test_strongswan_files_exists(host):
+    strongswan_files = [
+        "/etc/strongswan/ipsec.secrets",
+        "/etc/strongswan/ipsec.conf",
+        "/etc/strongswan/ipsec.d/certs/server.crt",
+        "/etc/strongswan/ipsec.d/private/server.key"
+    ]
+    for fl in strongswan_files:
+        assert host.file(fl).exists
+
+
+def test_kernel_parameters(host):
+    assert host.sysctl("net.ipv4.conf.all.accept_redirects") == 0
+    assert host.sysctl("net.ipv4.conf.all.send_redirects") == 0
+    assert host.sysctl("net.ipv4.ip_forward") == 1
+
+
+def test_strongswan_service_running(host):
+    svc_name = host.service(component_name)
+    assert svc_name.is_running
+    assert svc_name.is_enabled
+    assert host.socket("udp://0.0.0.0:500").is_listening
+    assert host.socket("udp://0.0.0.0:4500").is_listening
